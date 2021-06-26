141428694288c96a3fd8cc5846516e32fe202bd0
Federico <matiasiman@gmail.com>
2021-06-26 20:58:17 -0300

    change

--- test/tiny_mc_gpu.cu
+++ test/tiny_mc_gpu.cu
@@ -189,21 +189,11 @@ int main()
     (void)run_gpu_tiny_mc(heat, PHOTONS);
     double end = wtime();
     assert(start <= end);
-    double elapsed = (end - start) * 1000.0;
+    double elapsed = (end - start);
 
-    if (verbose) {
-        printf("# Radius\tHeat\n");
-        printf("# [microns]\t[W/cm^3]\tError\n");
-        float t = 4.0f * M_PI * powf(MICRONS_PER_SHELL, 3.0f) * PHOTONS / 1e12;
-        for (unsigned int i = 0; i < SHELLS - 1; ++i) {
-            printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
-                   heat[i][0] / t / (i * i + i + 1.0 / 3.0),
-                   sqrt(heat[i][1] - heat[i][0] * heat[i][0] / PHOTONS) / t / (i * i + i + 1.0f / 3.0f));
-        }
-        printf("# extra\t%12.5f\n", heat[SHELLS - 1][0] / PHOTONS);
-    }
-    printf("+>> %lf ms\n", elapsed);
-    printf("+>>> %lf photons per millisecond\n", 1e-3 * PHOTONS / (elapsed / 1000.0));
+    printf("%lf\n", 1e-3 * PHOTONS / elapsed);
+
+
 
     return 0;
 }
