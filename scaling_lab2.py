7c083accf35fe258a2e8accde46cd801dab6743a
pedro98pedro <pedro98escalera@gmail.com>
2021-06-27 20:30:22 -0300

    Update scaling_lab2.py

--- scaling_lab2.py
+++ scaling_lab2.py
@@ -16,12 +16,12 @@ def runScale():
     #randomSeed = random.randint(0, 4294967295)
     results = []
     execute(f'cd {path} && make clean')
-    for i in range(32768, 3276800, 65536):
+    for i in range(32768, 327680, 32768):
         execute(f'cd {path} && make cleanMain')
         execute(f"cd {path} && make EXTRA='-DPHOTONS={i}'")
         temp = []
         for j in range(3):
-            temp.append(float(executeWithOutput(f'cd {path} && ./tiny_cuda')))
+            temp.append(float(executeWithOutput(f'cd {path} && ./tiny_cuda_pro')))
         results.append([i, sum(temp)/len(temp)])
     with open(f"{path}/results_sca_tinyCudav2.txt", "a") as f:
         f.write(f"\n\nResults on {datetime.now()}\n")
